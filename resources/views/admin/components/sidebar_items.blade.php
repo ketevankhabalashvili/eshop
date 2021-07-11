dd2f0a14c3a7359e031234bc57b84ff1c2c090e6
ketevankhabalashvili <khabalashviliketevan@gmail.com>
2021-07-11 22:31:22 +0400

    updated sidebar items

--- resources/views/admin/components/sidebar_items.blade.php
+++ resources/views/admin/components/sidebar_items.blade.php
@@ -1,45 +1,45 @@
 <div class="sidebar-wrapper" id="sidebar-wrapper">
     <ul class="nav">
-        <li class="active">
-            <a href="/dashboard">
+        <li @if(Route::currentRouteName() == 'admin.main') class="active" @endif>
+            <a @if (Route::has('admin.main')) href="{{ route('admin.main') }}" @endif>
                 <i class="now-ui-icons design_app"></i>
                 <p>{{ __('Home') }}</p>
             </a>
         </li>
-        <li>
-            <a href="/role-register">
+        <li @if(Route::currentRouteName() == 'admin.users.index') class="active" @endif>
+            <a @if (Route::has('admin.users.index')) href="{{ route('admin.users.index') }}" @endif>
                 <i class="fa fa-users"></i>
                 <p>{{ __('Users') }}</p>
             </a>
         </li>
-        <li>
-            <a href="/products">
+        <li @if(Route::currentRouteName() == 'admin.categories.index') class="active" @endif>
+            <a @if (Route::has('admin.categories.index')) href="{{ route('admin.categories.index') }}" @endif>
+                <i class="fa fa-sliders-h"></i>
+                <p>{{ __('Categories') }}</p>
+            </a>
+        </li>
+        <li @if(Route::currentRouteName() == 'admin.products.index') class="active" @endif>
+            <a @if (Route::has('admin.products.index')) href="{{ route('admin.products.index') }}" @endif>
                 <i class="fa fa-database"></i>
                 <p>{{ __('Products') }}</p>
             </a>
         </li>
-        <li>
-            <a href="/sliders">
-                <i class="fa fa-sliders-h"></i>
-                <p>სლაიდერი</p>
-            </a>
-        </li>
-        <li>
-            <a href="/offers">
+        <li @if(Route::currentRouteName() == 'admin.orders.index') class="active" @endif>
+            <a @if (Route::has('admin.orders.index')) href="{{ route('admin.orders.index') }}" @endif>
                 <i class="fa fa-handshake"></i>
-                <p>შეთავაზებები</p>
+                <p>{{ __('Orders') }}</p>
             </a>
         </li>
-        <li>
-            <a href="/suggestions">
-                <i class="fa fa-hourglass"></i>
-                <p>შეთავაზება</p>
+        <li @if(Route::currentRouteName() == 'admin.advertisements.index') class="active" @endif>
+            <a @if (Route::has('admin.advertisements.index')) href="{{ route('admin.advertisements.index') }}" @endif>
+                <i class="fa fa-ad"></i>
+                <p>{{ __('Advertisements') }}</p>
             </a>
         </li>
-        <li>
-            <a href="/images">
+        <li @if(Route::currentRouteName() == 'admin.productImages.index') class="active" @endif>
+            <a @if (Route::has('admin.productImages.index')) href="{{ route('admin.productImages.index') }}" @endif>
                 <i class="fa fa-images"></i>
-                <p>ფოტოები</p>
+                <p>{{ __('Medias and Files') }}</p>
             </a>
         </li>
     </ul>
