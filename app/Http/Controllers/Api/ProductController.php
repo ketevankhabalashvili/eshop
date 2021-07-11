10249f1e713943a6d795439a90f69dd786f7c6f8
ketevankhabalashvili <khabalashviliketevan@gmail.com>
2021-07-12 01:06:20 +0400

    created passport api

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::all();
+        return response([ 'products' =>
+            ProductResource::collection($products),
+            'message' => 'Successful'], 200);
+    }
+
+}
